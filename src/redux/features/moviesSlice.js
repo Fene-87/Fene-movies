4f6955f40224eaf1362544ebd2e61aa1b869773b
Fene-87 <fenekayas87@gmail.com>
2023-03-21 17:50:30 +0300

    Create and style navbar

--- src/redux/features/moviesSlice.js
+++ src/redux/features/moviesSlice.js
@@ -1,13 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   movieList: [],
   status: 'idle',
 };
 
+export const fetchMovies = createAsyncThunk('movies/fetchMovies', async (show) => {
+  try {
+    const response = await axios.get(`https://api.tvmaze.com/search/shows?q=${show}`);
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+})
+
 const moviesSlice = createSlice({
   name: 'movies',
   initialState,
+  extraReducers(builder) {
+    builder
+      .addCase(fetchMovies.pending, (state) => ({
+        ...state,
+        status: 'laoding',
+      }))
+      .addCase(fetchMovies.fulfilled, (state, { payload }) => {
+        const keys = Object.keys(payload);
+        const tempArray = [];
+        keys.forEach((key) => {
+          tempArray.push(payload[key]);
+        })
+        state.movieList = [...tempArray];
+        state.status = 'successful';
+      })
+      .addCase(fetchMovies.rejected, (state, action) => ({
+        ...state,
+        status: 'failed',
+      }))
+  }
 });
 
 export default moviesSlice.reducer;
